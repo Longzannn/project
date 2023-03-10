51bfa1fc4bda6d14e3baa24675df24b20b78cc7a
Longzannn <luczanthien@gmail.com>
2023-03-10 22:39:39 +0700

    Fix giao diện

--- Views/pages/user/home/handleCardSlider.js
+++ Views/pages/user/home/handleCardSlider.js
@@ -1,17 +1,29 @@
-document.getElementById("next").onclick = function () {
-    const widthItem = document.querySelector(".swipper-slide").offsetWidth;
-    document.querySelector(".swipper").scrollLeft += (widthItem * 4);
-};
-document.getElementById("prev").onclick = function () {
-    const widthItem = document.querySelector(".swipper-slide").offsetWidth;
-    document.querySelector(".swipper").scrollLeft -= (widthItem * 4);
+var swipper = document.querySelector(".swipper")
+const widthItem = document.querySelector(".swipper-slide").offsetWidth
+
+var btnNext = document.getElementById("next")
+var btnPrev = document.getElementById("prev")
+
+btnNext.onclick = function () {
+    swipper.scrollLeft += (widthItem * 4);
 };
 
-document.getElementById("next-2").onclick = function () {
-    const widthItem2 = document.querySelector(".swipper-slide-2").offsetWidth;
-    document.querySelector(".swipper-2").scrollLeft += (widthItem2 * 4);
+btnPrev.onclick = function () {
+    swipper.scrollLeft -= (widthItem * 4);
 };
-document.getElementById("prev-2").onclick = function () {
-    const widthItem2 = document.querySelector(".swipper-slide-2").offsetWidth;
-    document.querySelector(".swipper-2").scrollLeft -= (widthItem2 * 4);
+
+
+// ----------------------------------------
+
+var swipper2 = document.querySelector(".swipper-2")
+const widthItem2 = document.querySelector(".swipper-slide-2").offsetWidth
+
+var btnNext2 = document.getElementById("next-2")
+var btnPrev2 = document.getElementById("prev-2")
+
+btnNext2.onclick = function () {
+    swipper2.scrollLeft += (widthItem2 * 4);
+};
+btnPrev2.onclick = function () {
+    swipper2.scrollLeft -= (widthItem2 * 4);
 };
